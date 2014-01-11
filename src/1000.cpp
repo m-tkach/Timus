3403382c630c8856393c9fad60b6fa7b21512146
Michael Tkach <x1mike7x@gmail.com>
2014-01-11 02:09:42 +0200

    Add 1000. A+B Problem

--- src/1000.cpp
+++ src/1000.cpp
@@ -1,10 +1,13 @@
 #include <bits/stdc++.h>
 
 
-namespace task0000
+namespace task1000
 {
     int solve(std::istream & in, std::ostream & out)
     {
+        int a, b;
+        in >> a >> b;
+        out << a + b << std::endl;
         return 0;
     }
 }
@@ -18,20 +21,20 @@ int main()
 #ifdef ONLINE_JUDGE
     do
     {
-        (void)task0000::solve(std::cin, std::cout);
+        (void)task1000::solve(std::cin, std::cout);
     }
     while (0);
 #else
     do
     {
-        std::ifstream fin("../input/0000.in");
+        std::ifstream fin("../input/1000.in");
         if (!(fin.is_open()))
         {
             std::cerr << "The input file isn't found!\n";
             break;
         }
-        std::ofstream fout("../output/0000.out");
-        int status = task0000::solve(fin, fout);
+        std::ofstream fout("../output/1000.out");
+        int status = task1000::solve(fin, fout);
         std::cout << status << std::endl;
         fout.close();
         fin.close();
