7d3a324c1ba8319b559fa57ca94fd3c22c17e013
unknown <m.tkach@m-tkach-d.surc.kiev.ua>
2014-01-13 19:25:26 +0200

    Add blank for 1590

---
+++ src/1590.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+
+namespace task1590
+{
+    int solve(std::istream & in, std::ostream & out)
+    {
+        return 0;
+    }
+}
+
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    srand((unsigned)time(0));
+
+#ifdef ONLINE_JUDGE
+    do
+    {
+        (void)task1590::solve(std::cin, std::cout);
+    }
+    while (0);
+#else
+    do
+    {
+        std::ifstream fin("../input/1590.in");
+        if (!(fin.is_open()))
+        {
+            std::cerr << "The input file isn't found!\n";
+            break;
+        }
+        std::ofstream fout("../output/1590.out");
+        int status = task1590::solve(fin, fout);
+        std::cout << status << std::endl;
+        fout.close();
+        fin.close();
+    }
+    while (0);
+#endif // ONLINE_JUDGE
+
+    return 0;
+}
